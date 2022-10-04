3014a58b6f23bd8d2e43b9a37d1875a2eec189c7
eduard-mirea <alumir79@ieselcaminas.org>
2022-10-04 23:57:35 +0200

    Initial commit

---
+++ src/Ejemplo7.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Ejemplo7 {
+    public static void main(String[] args) {
+        int number = 1852;
+        float   miles;
+        float   meters;
+
+
+    }
+}
